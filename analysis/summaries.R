42975a45513f57396c4c4fc970f4cad0b4b0d7ab
Abhijit Dasgupta <aikidasgupta@gmail.com>
2020-02-14 14:02:16 -0500

    First pass at summaries

--- analysis/summaries.R
+++ analysis/summaries.R
@@ -1,12 +1,28 @@
 library(readxl)
 library(tidyverse)
 library(glue)
+library(fs)
 
+# Documenting version of data we're using ---------------------------------
+data_date <- tibble(fname = dir_ls('data/raw', glob = '*.zip')) %>%
+  separate(fname, c('p1','p2','dt','tm'), sep = '_', remove = T) %>%
+  select(-p1, -p2) %>%
+  mutate(tm = str_remove(tm, '.zip'),
+         tm = as.numeric(tm)) %>% #,
+         # tm = substr(as.POSIXct(sprintf('%4.0f', tm), format = "%H%M"), 12,16)) %>%
+  arrange(desc(dt), desc(tm)) %>%
+  slice(1)
+glue_data(data_date, "The version of data we're using is from {dt} {substr(as.POSIXct(as.character(tm), format = '%H%M'), 12,16)}")
+
+
+# reading data dictionary -------------------------------------------------
 
 data_dict <- read_excel('background/f-6-337-13136911_ZH2vHUc6_111319_CARRA_Registry_11.0_DataDictionary_DCRI_SxxX.xlsx')
 names(data_dict) = make.names(names(data_dict))
 
 write.table(data_dict, sep = '\t', file = 'background/f-6-337-13136911_ZH2vHUc6_111319_CARRA_Registry_11.0_DataDictionary_DCRI_SxxX.tsv')
+
+
 
 #' Principle 1 : 20% to 75% of children with SLE will develop nephritis
 #' From the data dictionary, this question is answered in SLICC
@@ -17,7 +33,7 @@ write.table(data_dict, sep = '\t', file = 'background/f-6-337-13136911_ZH2vHUc6_
 glue_data(data_dict %>% filter(Variable.Name=='SLICC00'),
           'Variable {Variable.Name}: {Variable.Description}')
 
-all_rows <- read_csv('data/raw/all_rows_data_2020-01-29_1238.csv')
+all_rows <- read_csv('data/raw/all_rows_data_2020-01-31_1545.csv')
 all_rows %>% filter(varName=='SLICC00') %>%
   count(conceptValue) %>%
   mutate(perc = n/sum(n)*100)
@@ -30,6 +46,8 @@ all_rows %>% filter(varName=='SLICC00') %>%
 #'
 #' Biopsy form, WHO1-6, ISNRPS1-6
 #' Either/or WHO2-6, ISNRPS2-6 = LN
+
+
 
 #' Principle 4: Short term renal outcomes are worse in blacks of African American heritage
 
