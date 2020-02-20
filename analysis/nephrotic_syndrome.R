b2f9451ab4a266e314992438c21235eacba93c25
Abhijit Dasgupta <aikidasgupta@gmail.com>
2020-02-20 08:24:49 -0500

    nephrotic syndrome

---
+++ analysis/nephrotic_syndrome.R
@@ -0,0 +1,129 @@
+#' ---
+#' title: Defining nephrotic syndrome in the data
+#' author: Abhijit Dasgupta
+#' date: February 20, 2020
+#' output:
+#'   html_document:
+#'     code_folding: hide
+#'     toc: true
+#'     toc_depth: 2
+#'     toc_float: true
+#'     theme: sandstone
+#'     highlight: zenburn
+#' ---
+#'
+#+ preamble, include = F
+knitr::opts_chunk$set(message = F, warning = F)
+pkgs <- c('tidyverse','janitor','here','vroom','knitr','kableExtra')
+library(pacman)
+p_load(char=pkgs)
+#'
+#' <hr/>
+#' # Summary
+#'
+#' We have a paucity of data that can help us define **nephrotic syndrome** among the study
+#' subjects. The availability of urine protein:creatinine ratio is limited, and it appears that
+#' serum albumin is not measured on these subject.
+#'
+#' How can we proceed? Does this mean reaching out to the Boston folks, or are we just stuck.
+#' Please double check my variable extractions to see that I'm not missing obvious variables that
+#' are available in the dataset
+#'
+#' <hr/>
+#'
+#' # Definition
+#'
+#' The definition of nephrotic syndrome is (Kathleen, 2/19/2020):
+#'
+#' 1.  The presence of nephrotic range proteinuria, which is a urine
+#' protein:creatinine ratio \>200 mg/mmol or if there is a 24 hour
+#' urine instead of a urine protein:creatinine ratio, it would be a 24
+#' hour protein excretion greater than protein \>50 mg/kg (\>40 mg/m^2^
+#'                                                            per hour) in 24 hours.
+#' 2.  Hypoalbuminemia (an albumin less than 2.5 g/dL)
+#'
+#' # Availability of data
+#'
+#' ## Urinanalysis
+#'
+tribble(~Variable, ~Description, ~Form, ~Table, ~Type,
+        "SPOTURIN", "Urine protein to creatinine ratio (UPC) in last 30 days","Visit", "URINE","Category",
+        "URINRATO", "Urine Protein to creatinine ratio", "Visit", "URINE", "Numeric",
+        "PROT30", "Proteinuria in last 30 days", "Visit", "URINE", 'Category',
+        'DIPSTICK','Dipstick: Protein','Visit','URINE', 'Category') %>%
+  kable(caption = 'Available relevant variables from urine analysis') %>%
+  kable_styling(bootstrap_options = 'striped')
+#'
+#' There does not appear to be any raw protein or creatinine numbers from the urinanalysis.
+#' The protenuria and protein measurements are categorical.
+#'
+#' There appears to be rather low prevalence of these measures among the subjects:
+
+all_subjects <- vroom(here('data/raw/all_rows_data_2020-01-31_1545.csv'),
+                      col_select = c(subjectId, visit = folderName)) %>%
+  distinct() %>%
+  mutate(folderName = fct_relevel(visit, 'Baseline','6 month','12 month','18 month','24 month')) %>%
+  select(-folderName)
+
+urine <- vroom(here('data/raw/urine_data_2020-01-31_1545.csv'))
+urine <- urine %>%
+  select(subjectId, visit = folderName, DIPSTICK, PROT30, URINRATO, RATIOUNT, SPOTURIN)
+
+out <- all_subjects %>% left_join(urine)
+
+out %>% filter(visit %in% c('Baseline','6 month','12 month','18 month','24 month')) %>%
+  mutate(visit = fct_relevel(visit, 'Baseline',paste(c(6,12,18,24), 'month'))) %>%
+  group_by(visit) %>%
+  summarise(N = n(), perc_missing_UPC = 100*mean(is.na(URINRATO))) %>%
+  kable(col.names = c('Visit','N','Percent missing UPC'), digits=2) %>%
+  kable_styling(bootstrap_options = 'striped', full_width = F)
+
+#' The UPC is also categorized as < 0.5 mg/mg and >= 0.5 mg/mg.
+
+out %>%
+  filter(visit %in% c('Baseline',paste(c(6,12,18,24),'month'))) %>%
+  mutate(visit = fct_relevel(visit, 'Baseline','6 month')) %>%
+  tabyl(SPOTURIN, visit) %>%
+  adorn_percentages('col') %>%
+  adorn_totals('row') %>%
+  adorn_pct_formatting() %>%
+  adorn_ns() %>%
+  adorn_title('combined') %>%
+  kable(caption = 'Frequency of SPOTURIN categories by visit') %>%
+  kable_styling() %>%
+row_spec(row=5, bold = TRUE)
+
+#' The data on proteinuria is even sparser.
+out %>%
+  filter(visit %in% c('Baseline',paste(c(6,12,18,24),'month'))) %>%
+  mutate(visit = fct_relevel(visit, 'Baseline','6 month')) %>%
+  tabyl(PROT30, visit) %>%
+  adorn_percentages('col') %>%
+  adorn_totals('row') %>%
+  adorn_pct_formatting() %>%
+  adorn_ns() %>%
+  adorn_title('combined') %>%
+  kable(caption = 'Frequency of proteinuria categories by visit') %>%
+  kable_styling() %>%
+  row_spec(row = 5, bold = TRUE)
+
+#' ## Blood analysis
+#'
+#' It does not appear that serum albumin was tested for these subjects. The available
+#' assays from blood samples, as far as I can tell,  are given below.
+data_dict <- readxl::read_excel(here('background/f-6-337-13136911_ZH2vHUc6_111319_CARRA_Registry_11.0_DataDictionary_DCRI_SxxX.xlsx')) %>%
+  clean_names()
+
+data_dict %>%
+  filter(table_name=='LABS',data_type=='Text', !str_detect(variable_description, 'Upper')) %>%
+  select(starts_with('variable')) %>%
+  kable(format='html', col.names = snakecase::to_sentence_case(names(.))) %>%
+  kable_styling(bootstrap_options = 'striped')
+
+#' # Session information
+#'
+#' This analysis was done using `r R.version$version.string` and the following packages
+tibble(pkg = pkgs) %>%
+  mutate(version = map_chr(pkg, ~as.character(packageVersion(.)))) %>%
+  kable(col.names = c('Packages','Version')) %>%
+  kable_styling(full_width = F)
---
+++ background/Definitions.md
@@ -0,0 +1,61 @@
+**Golden Rules of Lupus Nephritis Outcomes **
+
+*Principles *
+
+1\) 20%-75% of cSLE patients will develop nephritis
+
+2\) 82% of LN in cSLE develops within the first year of diagnosis; 92%
+within 2 years
+
+3\) Class V LN more often presents with nephrotic syndrome than class III
+or IV
+
+[Nephrotic Syndrome]{.underline}:
+
+1.  The presence of nephrotic range proteinuria, which is a urine
+    protein:creatinine ratio \>200 mg/mmol or if there is a 24 hour
+    urine instead of a urine protein:creatinine ratio, it would be a 24
+    hour protein excretion greater than protein \>50 mg/kg (\>40 mg/m^2^
+    per hour) in 24 hours.
+
+2.  Hypoalbuminemia (an albumin less than 2.5 g/dL)
+
+4\) Short-term renal outcomes are worse in African Americans
+
+5\) Short-term renal outcomes are worse in patients who present with GFR
+\<60 mL/min/1.73 m2 and/or nephrotic range proteinuria
+
+[Short Term Renal Outcomes]{.underline}:
+
+-   GFR will be separated into states for analysis:
+
+    -   State 1: \>60ml/min/1.73 m2
+
+    -   State 2: 30-60ml/min/1.73 m2
+
+    -   State 3: \<30ml/min1.73 m2
+
+    -   Assess change in GFR- initial GFR vs most recent GFR
+
+-   Occurrence of remission:
+
+-   Creatinine within normal range:
+
+  Age                              Creatinine (serum)   
+  -------------------------------- -------------------- --------------
+  Child (\<12 years old)           0.3-0.7 mg/dL        27-62 umol/L
+  Adolescent (12-17 years old)     0.5-1.0 mg/dL        44-88 umol/L
+  Adult (18 years old and older)   0.6-1.1 mg/dL        53-97 umol/L
+
+-   urine protein:creatinine ratio\<0.2
+
+-   urine red blood cells\<10/high powered field
+
+<!-- -->
+
+-   Occurrence of end-stage renal disease, transplant, and/or dialysis.
+
+6\) Rituximab is used as a steroid-sparing agent for induction in
+proliferative LN
+
+-   Usage in class III-IV vs class V lupus nephritis
